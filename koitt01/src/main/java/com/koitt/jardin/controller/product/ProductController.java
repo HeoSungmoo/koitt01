d3f0daa904b41bf42c579510c6d4ac04bb94efad
Park-Jun-Kyu <dailykq@naver.com>
2020-06-02 15:08:34 +0900

    박준규 test

--- koitt01/src/main/java/com/koitt/jardin/controller/product/ProductController.java
+++ koitt01/src/main/java/com/koitt/jardin/controller/product/ProductController.java
@@ -1,44 +1,57 @@
 package com.koitt.jardin.controller.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.koitt.jardin.dto.product.ProductDTO;
 import com.koitt.jardin.service.product.ProductService;
 
 @Controller
 public class ProductController {
 
-	ProductService pService;
+	@Autowired
+	ProductService productService;
 
+	// 제품리스트 목록페이지
 	@RequestMapping("list")
-	public String List() {
-
+	public String list(Model model) {
+		model.addAttribute("list", productService.list());
 		return "product/list";
 	}
 
+	// 제품의 상세내용 ( condent_view)
 	@RequestMapping("detail")
-	public String Detail() {
-
+	public String detail(Model model, int product_No) {
+		model.addAttribute("detail", productService.detail(product_No));
 		return "product/detail";
 	}
 
+	// 질문과 답변 작성란 ::추후에
 	@RequestMapping("Pinquiry")
-	public String Inquiry() {
+	public String inquiry() {
 		return "product/inquiry";
 	}
 
+	// 포토리뷰 작성란
 	@RequestMapping("photo")
-	public String Photo() {
+	public String photo(ProductDTO ProductDto) {
+		productService.photo(ProductDto);
 		return "product/photo";
 	}
 
+	// 구매후기 작성란
 	@RequestMapping("review")
-	public String Review() {
+	public String review(ProductDTO ProductDto) {
+		productService.review(ProductDto);
 		return "product/review";
 	}
 
 	@RequestMapping("search")
-	public String Search() {
+
+	public String search(Model model, int product_No) {
+		model.addAttribute("search", product_No);
 		return "product/search";
 	}
 
--- koitt01/src/main/java/com/koitt/jardin/dao/product/ProductDAO.java
+++ koitt01/src/main/java/com/koitt/jardin/dao/product/ProductDAO.java
@@ -1,5 +1,19 @@
 package com.koitt.jardin.dao.product;
 
+import java.util.List;
+
+import com.koitt.jardin.dto.product.ProductDTO;
+
 public interface ProductDAO {
+
+	public List<ProductDTO> list();
+
+	public ProductDTO detail(int product_No);
+
+	public void photo(ProductDTO ProductDto);
+
+	public void review(ProductDTO ProductDto);
+
+	public ProductDTO search(int product_No);
 
 }

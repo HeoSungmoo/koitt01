e9724741c83cbc6471ff151cd546fc3d25b09baa
yohan <yohan@DESKTOP-QTI9N85>
2020-06-02 10:42:14 +0900

    조요한 - 서블릿 컨텍스트에 데이터소스,sqlSession,sqlSessionFactory 추가, pom에 mybatis,jstl,

--- koitt01/src/main/java/com/koitt/jardin/controller/payment/PaymentController.java
+++ koitt01/src/main/java/com/koitt/jardin/controller/payment/PaymentController.java
@@ -1,5 +1,8 @@
 package com.koitt.jardin.controller.payment;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class PaymentController {
 
 }
--- koitt01/src/main/java/com/koitt/jardin/service/board/BoardService.java
+++ koitt01/src/main/java/com/koitt/jardin/service/board/BoardService.java
@@ -1,5 +1,11 @@
 package com.koitt.jardin.service.board;
 
+import java.util.List;
+
+import com.koitt.jardin.dto.board.BoardDTO;
+
 public interface BoardService {
+
+	public List<BoardDTO> notice();
 
 }

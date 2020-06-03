b06d56aad059dfb0e124985b8498f6accc69b632
cookieinseong <60763708+cookieinseong@users.noreply.github.com>
2020-06-03 13:44:27 +0900

    member랑 myPage DTO 수정 및 sql문 추가

--- koitt01/src/main/java/com/koitt/jardin/dao/member/MyPageDAO.java
+++ koitt01/src/main/java/com/koitt/jardin/dao/member/MyPageDAO.java
@@ -2,8 +2,8 @@ package com.koitt.jardin.dao.member;
 
 import java.util.List;
 
+import com.koitt.jardin.dto.member.InquiryDTO;
 import com.koitt.jardin.dto.member.MemberDTO;
-import com.koitt.jardin.dto.member.inquiry.InquiryDTO;
 
 public interface MyPageDAO {
 	// 1:1문의 리스트 출력

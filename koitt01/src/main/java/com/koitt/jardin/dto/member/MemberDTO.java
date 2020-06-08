bd4eeaf459729ea3bbff27e4e9b571561cb76f1a
cookieinseong <60763708+cookieinseong@users.noreply.github.com>
2020-06-08 15:48:19 +0900

    회원가입폼

--- koitt01/src/main/java/com/koitt/jardin/dto/member/MemberDTO.java
+++ koitt01/src/main/java/com/koitt/jardin/dto/member/MemberDTO.java
@@ -18,43 +18,17 @@ public class MemberDTO {
 	private String phone2; // 중간 휴대폰
 	private String phone3; // 마지막 핸드폰
 	private int sms; // 문자 수신
-	private int tel; // 유선 전화
+	private String tel;
+	private String tel1; // 첫번째 유선전화
+	private String tel2; // 두번째 유선전화
+	private String tel3; // 세번째 유선전화
 	private Timestamp birth; // 생일
+	private String year;
+	private String month;
+	private String day;
 	private int lunarSolar; // 양력,음력
 	private Timestamp joinDate; // 회원 가입 날짜
 	private Timestamp deleteDate; // 회원 탈퇴 날짜
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getPhone1() {
-		return phone1;
-	}
-
-	public void setPhone1(String phone1) {
-		this.phone1 = phone1;
-	}
-
-	public String getPhone2() {
-		return phone2;
-	}
-
-	public void setPhone2(String phone2) {
-		this.phone2 = phone2;
-	}
-
-	public String getPhone3() {
-		return phone3;
-	}
-
-	public void setPhone3(String phone3) {
-		this.phone3 = phone3;
-	}
 
 	public String getId() {
 		return id;
@@ -112,6 +86,38 @@ public class MemberDTO {
 		this.address = address;
 	}
 
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPhone1() {
+		return phone1;
+	}
+
+	public void setPhone1(String phone1) {
+		this.phone1 = phone1;
+	}
+
+	public String getPhone2() {
+		return phone2;
+	}
+
+	public void setPhone2(String phone2) {
+		this.phone2 = phone2;
+	}
+
+	public String getPhone3() {
+		return phone3;
+	}
+
+	public void setPhone3(String phone3) {
+		this.phone3 = phone3;
+	}
+
 	public int getSms() {
 		return sms;
 	}
@@ -120,12 +126,36 @@ public class MemberDTO {
 		this.sms = sms;
 	}
 
-	public int getTel() {
+	public String getTel() {
 		return tel;
 	}
 
-	public void setTel(int tel) {
+	public void setTel(String tel) {
 		this.tel = tel;
+	}
+
+	public String getTel1() {
+		return tel1;
+	}
+
+	public void setTel1(String tel1) {
+		this.tel1 = tel1;
+	}
+
+	public String getTel2() {
+		return tel2;
+	}
+
+	public void setTel2(String tel2) {
+		this.tel2 = tel2;
+	}
+
+	public String getTel3() {
+		return tel3;
+	}
+
+	public void setTel3(String tel3) {
+		this.tel3 = tel3;
 	}
 
 	public Timestamp getBirth() {
@@ -134,6 +164,30 @@ public class MemberDTO {
 
 	public void setBirth(Timestamp birth) {
 		this.birth = birth;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
 	}
 
 	public int getLunarSolar() {

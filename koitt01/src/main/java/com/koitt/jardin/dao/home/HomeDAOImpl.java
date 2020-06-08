972dda1c1dca629541b073bb222045c188dd60c3
HeoSungMoo <44401819+HeoSungmoo@users.noreply.github.com>
2020-06-08 18:42:58 +0900

    허성무 - 메인페이지(상품 및 데이터 블러오기 완료) new coffee mdchoice 만 남음

--- koitt01/src/main/java/com/koitt/jardin/dao/home/HomeDAOImpl.java
+++ koitt01/src/main/java/com/koitt/jardin/dao/home/HomeDAOImpl.java
@@ -6,7 +6,9 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.koitt.jardin.dto.board.NoticeDTO;
 import com.koitt.jardin.dto.home.BannerDTO;
+import com.koitt.jardin.dto.home.ProductTypeDTO;
 
 @Repository
 public class HomeDAOImpl implements HomeDAO {
@@ -14,8 +16,33 @@ public class HomeDAOImpl implements HomeDAO {
 	@Autowired
 	SqlSession sqlSession;
 
+	// 배너 리스트 불러오기
 	@Override
 	public List<BannerDTO> bannerList() {
-		return sqlSession.selectList("home.banner");
+		return sqlSession.selectList("home.bannerList");
+	}
+
+	// 베스트셀러 리스트 불러오기
+	@Override
+	public List<ProductTypeDTO> hitList() {
+		return sqlSession.selectList("home.hitList");
+	}
+
+	// 할인상품 리스트 불러오기
+	@Override
+	public List<ProductTypeDTO> saleList() {
+		return sqlSession.selectList("home.saleList");
+	}
+
+	// 고객센터 전화번호 불러오기
+	@Override
+	public String tel() {
+		return sqlSession.selectOne("home.tel");
+	}
+
+	// 공지사항 상위글 3개 불러오기
+	@Override
+	public List<NoticeDTO> notice() {
+		return sqlSession.selectList("home.notice");
 	}
 }

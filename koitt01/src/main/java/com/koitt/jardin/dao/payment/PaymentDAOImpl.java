681239ca2e0218ed73930993d365c310d70f6cc1
HeoSungMoo <44401819+HeoSungmoo@users.noreply.github.com>
2020-06-09 21:16:27 +0900

    허성무 - test

--- koitt01/src/main/java/com/koitt/jardin/dao/payment/PaymentDAOImpl.java
+++ koitt01/src/main/java/com/koitt/jardin/dao/payment/PaymentDAOImpl.java
@@ -1,5 +1,12 @@
 package com.koitt.jardin.dao.payment;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class PaymentDAOImpl implements PaymentDAO {
 
+	@Autowired
+	SqlSession sqlSession;
 }
